6e6c639ca7c129d51825e65652f48bd32a9f1964
RAKESH RACHAPUDI <rakeshrachapudi04@gmail.com>
2025-10-31 12:59:56 +0530

    Update SecurityConfig.java

--- realestate-backend/src/main/java/com/example/realestate/config/SecurityConfig.java
+++ realestate-backend/src/main/java/com/example/realestate/config/SecurityConfig.java
@@ -36,7 +36,6 @@ public class SecurityConfig {
                 "https://www.propertydealz.in",
                 "http://propertydealz.in",
                 "http://www.propertydealz.in",
-                "https://propertydealsdotcom.onrender.com/",
                 "http://localhost:3000"
         ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
